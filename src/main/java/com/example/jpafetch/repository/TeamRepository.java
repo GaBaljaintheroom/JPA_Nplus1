bd4deca454dade05b2d17ef0b8b7124faa189bea
GaBaljaintheroom <junsu1222@naver.com>
2024-02-13 22:48:54 +0900

    feat : Team 페이징 쿼리 작성

--- src/main/java/com/example/jpafetch/repository/TeamRepository.java
+++ src/main/java/com/example/jpafetch/repository/TeamRepository.java
@@ -2,12 +2,13 @@ package com.example.jpafetch.repository;
 
 import com.example.jpafetch.entity.Team;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
-
 
 
     @Query("select distinct t from Team t join fetch t.members")
@@ -16,4 +17,8 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     @EntityGraph(attributePaths = "members")
     @Query("select distinct t from Team t")
     List<Team> findAllByEntityGraph();
+
+    @Query("select distinct t from Team t join fetch t.members")
+    Page<Team> findAllJoinFetchWithPaging(Pageable pageable);
+
 }
