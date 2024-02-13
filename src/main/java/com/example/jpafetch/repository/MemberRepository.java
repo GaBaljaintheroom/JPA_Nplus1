eb153949d2992b3fb5ce4d47c1eea3f427558383
GaBaljaintheroom <junsu1222@naver.com>
2024-02-13 22:49:06 +0900

    feat : Member 페이징 쿼리 작성

--- src/main/java/com/example/jpafetch/repository/MemberRepository.java
+++ src/main/java/com/example/jpafetch/repository/MemberRepository.java
@@ -1,8 +1,14 @@
 package com.example.jpafetch.repository;
 
 import com.example.jpafetch.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("select distinct m from Member m join fetch m.team")
+    Page<Member> findAllJoinFetchWithPaging(Pageable pageable);
 
 }
