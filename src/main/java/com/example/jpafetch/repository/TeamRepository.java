fab35b173a6576e72e109a8d34b314f9e9c7fa77
GaBaljaintheroom <junsu1222@naver.com>
2024-02-17 23:36:24 +0900

    feat : JPQL 쿼리 추가 작성

--- src/main/java/com/example/jpafetch/repository/TeamRepository.java
+++ src/main/java/com/example/jpafetch/repository/TeamRepository.java
@@ -1,12 +1,15 @@
 package com.example.jpafetch.repository;
 
+import com.example.jpafetch.dto.TeamsDto;
 import com.example.jpafetch.entity.Team;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
@@ -21,4 +24,10 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     @Query("select distinct t from Team t join fetch t.members")
     Page<Team> findAllJoinFetchWithPaging(Pageable pageable);
 
+    @Query("select distinct t from Team t join fetch t.members join fetch t.sponsors where t.id = :teamId")
+    Optional<Team> findTeamWithMembersAndSponsorsByJoinFetch(@Param("teamId") Long teamId);
+
+    @Query("SELECT new com.example.jpafetch.dto.TeamsDto(t.name, m.name, s.name) " +
+        "FROM Team as t LEFT JOIN t.members as m LEFT JOIN t.sponsors as s where t.id =:teamId")
+    TeamsDto findTeamWithMembersAndSponsorsByLeftJoin(@Param("teamId") Long teamId);
 }
